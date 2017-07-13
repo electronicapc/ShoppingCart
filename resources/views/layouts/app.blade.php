305944b28418e1fadeedb461e2faad56f38dfa38
nelsonreyes <gunsnjrc_999@hotmail.com>
2017-07-12 21:57:30 -0500

    Correcion look and feel titulo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Softecol') }}</title>
+    <title>{{ config('Softecol', 'Softecol') }}</title>
 
     <!-- Styles -->
     <link href="{{  asset('css/app.css') }}" rel="stylesheet">
